71d7e7748c086643febc19a7add2154023a70bda
Abigail <abigailtauro@gmail.com>
2020-08-20 21:19:25 +0530

    dd changes to get status of COVID by country on day one

--- data_script.py
+++ data_script.py
@@ -7,6 +7,28 @@ if not os.path.exists('data'):
 
 req = requests.get("https://api.covid19api.com/")
 
-if req.status_code==200:
-    data = (json.loads(req.text)) #converts dict to text
-    print (data.keys())
+def base():
+    """
+    Gets the data from the base url
+    """
+    data = {}
+    req = requests.get(base_url)
+    if req.status_code==200:
+        data = (json.loads(req.text)) #converts dict to text
+
+    return data
+
+def get_day_one():
+    """
+    Gets the data for day one by country
+    """
+
+    url = "{}dayone/country/{}/status/confirmed".format(base_url, country)
+    data = {}
+    req = requests.get(url)
+
+    if req.status_code==200:
+        data = (json.loads(req.text))
+
+if __name__ == "__main__":
+    print (get_day_one("canada"))
